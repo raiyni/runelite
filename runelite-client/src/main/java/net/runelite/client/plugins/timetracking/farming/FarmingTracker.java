5c94a3ff8f367b666945dfd2a2b73a6bb98d57d7
raiyni <admin@ryoung.io>
2018-11-30 14:41:33 -0600

    undo auto format

--- runelite-client/src/main/java/net/runelite/client/plugins/timetracking/farming/FarmingTracker.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/timetracking/farming/FarmingTracker.java
@@ -58,7 +58,7 @@ public class FarmingTracker
 
 	@Inject
 	private FarmingTracker(Client client, ItemManager itemManager, ConfigManager configManager,
-						   TimeTrackingConfig config, FarmingWorld farmingWorld)
+		TimeTrackingConfig config, FarmingWorld farmingWorld)
 	{
 		this.client = client;
 		this.itemManager = itemManager;
