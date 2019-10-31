509a32d094506fcc75579d6aea51974c9c88f3aa
Ron Young <admin@ryoung.io>
2019-10-31 12:21:05 -0500

    grandexchange: rebuild GE item text on rebuild script event

--- runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangePlugin.java
@@ -54,13 +54,10 @@ import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.ConfigChanged;
 import net.runelite.api.events.FocusChanged;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
 import net.runelite.api.events.GrandExchangeOfferChanged;
 import net.runelite.api.events.MenuEntryAdded;
-import net.runelite.client.events.SessionClose;
-import net.runelite.client.events.SessionOpen;
-import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.events.ScriptCallbackEvent;
+import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetID;
 import net.runelite.api.widgets.WidgetInfo;
@@ -69,6 +66,8 @@ import net.runelite.client.account.AccountSession;
 import net.runelite.client.account.SessionManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.SessionClose;
+import net.runelite.client.events.SessionOpen;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.input.KeyManager;
 import net.runelite.client.input.MouseManager;
@@ -151,6 +150,9 @@ public class GrandExchangePlugin extends Plugin
 	private Widget grandExchangeText;
 	private Widget grandExchangeItem;
 	private Map<Integer, Integer> itemGELimits;
+
+	private int osbItem = -1;
+	private String osbText = "";
 
 	private GrandExchangeClient grandExchangeClient;
 
@@ -432,6 +434,11 @@ public class GrandExchangePlugin extends Plugin
 	@Subscribe
 	public void onScriptCallbackEvent(ScriptCallbackEvent event)
 	{
+		if (event.getEventName().equals("geBuilt"))
+		{
+			rebuildGeText();
+		}
+
 		if (!event.getEventName().equals("setGETitle") || !config.showTotal())
 		{
 			return;
@@ -472,8 +479,7 @@ public class GrandExchangePlugin extends Plugin
 		stringStack[stringStackSize - 1] += titleBuilder.toString();
 	}
 
-	@Subscribe
-	public void onGameTick(GameTick event)
+	public void rebuildGeText()
 	{
 		if (grandExchangeText == null || grandExchangeItem == null || grandExchangeItem.isHidden())
 		{
@@ -481,7 +487,6 @@ public class GrandExchangePlugin extends Plugin
 		}
 
 		final Widget geText = grandExchangeText;
-		final String geTextString = geText.getText();
 		final int itemId = grandExchangeItem.getItemId();
 
 		if (itemId == OFFER_DEFAULT_ITEM_ID || itemId == -1)
@@ -490,26 +495,38 @@ public class GrandExchangePlugin extends Plugin
 			return;
 		}
 
-		if (config.enableGELimits() && itemGELimits != null && !geTextString.contains(BUY_LIMIT_GE_TEXT))
+		String[] lines = geText.getText().split("<br>");
+		String text = lines[0];
+
+		if (config.enableGELimits() && itemGELimits != null)
 		{
 			final Integer itemLimit = itemGELimits.get(itemId);
 
 			// If we have item buy limit, append it
 			if (itemLimit != null)
 			{
-				final String text = geText.getText() + BUY_LIMIT_GE_TEXT + QuantityFormatter.formatNumber(itemLimit);
-				geText.setText(text);
+				text += BUY_LIMIT_GE_TEXT + QuantityFormatter.formatNumber(itemLimit);
 			}
 		}
 
-		if (!config.enableOsbPrices() || geTextString.contains(OSB_GE_TEXT))
+		geText.setText(text);
+		if (!config.enableOsbPrices())
 		{
 			// OSB prices are disabled or price was already looked up, so no need to set it again
 			return;
 		}
 
+		if (osbItem == itemId)
+		{
+			log.debug("OSB item price already looked up {}", itemId);
+			geText.setText(text + osbText);
+			return;
+		}
+
 		log.debug("Looking up OSB item price {}", itemId);
 
+		osbItem = itemId;
+		final String str = text;
 		executorService.submit(() ->
 		{
 			if (geText.getText().contains(OSB_GE_TEXT))
@@ -521,11 +538,13 @@ public class GrandExchangePlugin extends Plugin
 			try
 			{
 				final OSBGrandExchangeResult result = CLIENT.lookupItem(itemId);
-				final String text = geText.getText() + OSB_GE_TEXT + QuantityFormatter.formatNumber(result.getOverall_average());
-				geText.setText(text);
+				osbText = OSB_GE_TEXT + QuantityFormatter.formatNumber(result.getOverall_average());
+				geText.setText(str + osbText);
 			}
 			catch (IOException e)
 			{
+				osbText = "";
+				geText.setText(str);
 				log.debug("Error getting price of item {}", itemId, e);
 			}
 		});
